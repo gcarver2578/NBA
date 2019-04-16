ccafdcf93311ab8fe39b59b4e1ad2c6ba362f635
gcarver2578 <gcarver2578@student.rcschools.net>
2019-04-16 15:00:04 -0500

    just starting... this is going to take a while

--- nba.py
+++ nba.py
@@ -1,1 +1,22 @@
 # Gabriel Carver
+from tkinter import *
+
+class Start:
+    def __init__(self, root):
+        self.root = root
+        self.t_frame = Frame(self.root)
+        self.b_frame = Frame(self.root)
+        self.b1_frame = Frame(self.b_frame)
+        self.b2_frame = Frame(self.b_frame)
+        self.b3_frame = Frame(self.b_frame)
+        self.t_frame.pack(side=TOP)
+        self.b_frame.pack(side=TOP)
+        self.b1_frame.pack(side=TOP)
+        self.b2_frame.pack(side=TOP)
+        self.b3_frame.pack(side=TOP)
+
+
+def main():
+    root= Tk()
+    strt = Start(root)
+    root.mainloop()
